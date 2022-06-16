33ffac7811475ddc628a4d1a5835addb0aa691b0
prabhjot kaur <priya998815@gmail.com>
2022-06-16 11:35:12 +0530

    update serializer field

--- stockapp_project/stockapp/serializers.py
+++ stockapp_project/stockapp/serializers.py
@@ -6,13 +6,15 @@ from rest_framework import serializers
 
        
 class Categoryserilizer(serializers.ModelSerializer):
+    # product = serializers.HiddenField(default = 1)
     class Meta:
         model = Category
-        fields = ['product_code','packs_per_carton']
+        fields = ['product','product_code','packs_per_carton']
         depth = 1
 
 
 class Productserilizer(serializers.ModelSerializer):
+    barcode_number = serializers.HiddenField(default = 1)
     category_set = Categoryserilizer(many=True,read_only= True)
     class Meta:
         model = Product
@@ -29,4 +31,4 @@ class Transactionserilizer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
-        depth = 3
+        depth = 0
